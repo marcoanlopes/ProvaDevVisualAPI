1c43914852be706ad231e9fe727305db2466feff
Marco Lopes <marcoalopes@outlook.com>
2021-09-30 18:23:50 -0300

    Criação da model Person

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+namespace ProvaDevVisualAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Address { get; set; }
+    }
+}
